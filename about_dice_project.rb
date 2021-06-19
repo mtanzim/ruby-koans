809508d88bbed2e5018fa4c16ea22f1eb150a6dc
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-06-19 17:44:56 -0400

    76 pct

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -6,6 +6,21 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+
+  attr_reader :values
+
+  def initialize
+    @possible_range = (1...6)
+    @values = []
+  end
+
+  def roll(n)
+    @values = (1..n).map { |_| rand(@possible_range) }
+  end
+
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
