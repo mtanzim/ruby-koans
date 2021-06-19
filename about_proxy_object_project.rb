459477b0eb8070d2f7e824aa3135be2183449910
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-06-19 18:50:39 -0400

    98pct

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -13,9 +13,25 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_reader :messages
   def initialize(target_object)
     @object = target_object
     # ADD MORE CODE HERE
+    @messages = []
+  end
+
+  def method_missing(method_name, *args)
+    @messages << method_name
+    @object.send(method_name, *args)
+  end
+
+  def called?(name)
+    @messages.include?(name)
+  end
+
+  def number_of_times_called(name)
+    count = @messages.tally[name]
+    count ? count : 0
   end
 
   # WRITE CODE HERE
